2dd4a779b9e187692921bf658ba76aff6dcf4946
AngadTiwari <angad.tiwari@hotmail.com>
2026-01-30 14:21:45 +0530

    fix(monitoring): replace hardcoded connection string with environment variable

--- src/monitor/index.js
+++ src/monitor/index.js
@@ -2,8 +2,9 @@ import { useAzureMonitor } from "@azure/monitor-opentelemetry";
 
 console.log("Initializing Azure Monitor...");
 console.log("Using Connection String:", process.env.APPLICATIONINSIGHTS_CONNECTION_STRING);
+console.log("Monitoring setup in progress...");
 useAzureMonitor({
   azureMonitorExporterOptions: {
-    connectionString: "InstrumentationKey=bd104e2f-fada-4bb7-9628-7d3a234f848f;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/;ApplicationId=c3cf30ef-c36a-4790-a3bf-a40259582d4b"
+    connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING
   }
 });
